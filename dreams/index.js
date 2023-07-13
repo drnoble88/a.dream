3857a063312cd6c246bc7dceb59095488fea06a7
gux <gabbyburgard@yahoo.com>
2023-07-13 17:19:04 -0600

    cleanup

--- dreams/index.js
+++ dreams/index.js
@@ -13,10 +13,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded request bodies
 
 app.get('/destination', (req, res) => {
-    const Figname = 'Paris'
+    const Figname = req.query
     const fetchData = {
         method: 'GET',
-        url: `https://api.pexels.com/v1/search?query=${Figname}`,
+        url: `https://api.pexels.com/v1/search`,
+        params: {query: Figname} ,
         headers: {
             "Authorization": process.env.REACT_APP_API_KEY,
         },
@@ -28,57 +29,6 @@ app.get('/destination', (req, res) => {
         console.error(error)
     })
 
-}
-)
-
-app.get('/figurehistory', (req, res) => {
-    const Figname = req.query.text
-    const fetchData = {
-        method: 'GET',
-        url: `https://api.api-ninjas.com/v1/historicalevents`,
-        params: { text: Figname },
-        headers: {
-            "X-Api-Key": process.env.REACT_APP_API_KEY,
-        },
-    };
-
-    axios.request(fetchData).then((response) => {
-        res.json(response.data[0])
-    }).catch((error) => {
-        console.error(error)
-    })
-
-}
-)
-
-app.post('/inform', (req, res) => {
-    async function postData() {
-        
-        const history2 = req.body.data.messages
-        try {
-
-            const url = 'https://api.openai.com/v1/chat/completions';  
-            let data = {
-                'model': "gpt-3.5-turbo",
-                "messages":[{ "role": "user", "content": history2 }]
-            };
-            const headers = {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-            };
-
-            const response = await axios.post(url, data, { headers });
-
-            res.json(response.data);
-
-
-        } catch (error) {
-            console.error('Error:', error.message);
-            res.status(500).json({ error: 'An error occurred' });
-        }
-
-    }
-    postData();
 }
 )
 
--- dreams/src/Destination.js
+++ dreams/src/Destination.js
@@ -5,10 +5,12 @@ import axios from 'axios';
 
 function Destination() {
     const [Image, setImage] = useState("")
+    const [Figname, setFigname] = useState("London")
 
     const fetchImage = {
         method: 'GET',
         url: 'http://localhost:8000/destination',
+        query: Figname,
     }
 
     useEffect(() =>{
@@ -27,7 +29,7 @@ function Destination() {
 
 <div>
 
-<p className="destination-vibes">Destination Vibes</p>
+<h2 className="destination-vibes">Destination Vibes</h2>
 <div id="myCarousel" className="carousel slide">
             <div className="carousel-indicators">
               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -80,32 +82,7 @@ function Destination() {
               <span className="visually-hidden">Next</span>
             </button>
           </div>
-          <div id="myCarousel2" className="carousel slide">
-            <div className="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <img src="./philhub1.png"  className="d-block" id="first-phil" alt="..."/>
-              </div>
-              <div className="carousel-item">
-                <img src="./philhub2.png" className="d-block" id="second-phil" alt="..."/>
-              </div>
-              <div className="carousel-item">
-                <img src={Image} className="d-block" alt="..."/>
-              </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
+         
 </div>
 
 </>
